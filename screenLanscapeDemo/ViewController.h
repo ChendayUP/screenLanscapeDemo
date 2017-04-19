14f9bd5713241aec16c4aea12275f921b8293088
ChenDayUP <mac@apple.local>
2017-04-19 16:36:22 +0800

    init commit

---
+++ screenLanscapeDemo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  screenLanscapeDemo
+//
+//  Created by mac on 2017/4/18.
+//  Copyright © 2017年 myself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, assign) BOOL isFullScreen;
+
+@end
+
---
+++ screenLanscapeDemo/ViewController.m
@@ -0,0 +1,50 @@
+//
+//  ViewController.m
+//  screenLanscapeDemo
+//
+//  Created by mac on 2017/4/18.
+//  Copyright © 2017年 myself. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"test";
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)change:(id)sender {
+
+    if ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait) {
+        self.isFullScreen = YES;
+        NSNumber *orientationUnknown = [NSNumber numberWithInt:UIInterfaceOrientationUnknown];
+        [[UIDevice currentDevice] setValue:orientationUnknown forKey:@"orientation"];
+        
+        NSNumber *orientationTarget = [NSNumber numberWithInt:UIInterfaceOrientationLandscapeRight];
+        [[UIDevice currentDevice] setValue:orientationTarget forKey:@"orientation"];
+    } else {
+        self.isFullScreen = NO;
+        NSNumber *orientationUnknown = [NSNumber numberWithInt:UIInterfaceOrientationUnknown];
+        [[UIDevice currentDevice] setValue:orientationUnknown forKey:@"orientation"];
+        
+        NSNumber *orientationTarget = [NSNumber numberWithInt:UIInterfaceOrientationPortrait];
+        
+        [[UIDevice currentDevice] setValue:orientationTarget forKey:@"orientation"];
+    }
+
+}
+
+@end
